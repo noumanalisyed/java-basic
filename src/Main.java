079250a13ff9e2ac1445e4331bc56c4aa6853289
noumanalisyed <nouman.ali.syed@gmail.com>
2023-07-05 15:04:06 +0500

    Define 2 Objects of Complex No in Driver class one is with default value and the other with user defined values,display both objects using the print function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ComplexNo complexNo1 = new ComplexNo();
+        complexNo1.print();
+        ComplexNo complexNo2 = new ComplexNo(4,5);
+        complexNo2.print();
     }
 }
